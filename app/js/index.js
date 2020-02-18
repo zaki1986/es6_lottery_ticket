27c53f5148db206669e039c42f8e9eba5cc1346c
zhang_chuang <shark1122!>
2020-02-18 09:05:30 +0800

    done

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,4 @@
 // import 'babel-polyfill'; //ES2017 一些字符串扩展方法补丁
 import Lottery from './lottery';
+
+const syy=new Lottery();
--- app/js/lottery/timer.js
+++ app/js/lottery/timer.js
@@ -2,36 +2,34 @@ class Timer {
   countdown(end, update, handle) {
     const now = new Date().getTime();
     const self = this;
-
-    if (end - now) {
+    if (now - end > 0) {
       handle.call(self);
     } else {
-      let lastTime = end - now;
+      let last_time = end - now;
       const px_d = 1000 * 60 * 60 * 24;
       const px_h = 1000 * 60 * 60;
       const px_m = 1000 * 60;
       const px_s = 1000;
-      let d = Math.floor(lastTime / px_d);
-      let h = Math.floor(lastTime - d * px_d / px_h);
-      let m = Math.floor(lastTime - d * px_d - h * px_h / px_m);
-      let s = Math.floor(lastTime - d * px_d - m * px_m / px_s);
-      let result = [];
+      let d = Math.floor(last_time / px_d);
+      let h = Math.floor((last_time - d * px_d) / px_h);
+      let m = Math.floor((last_time - d * px_d - h * px_h) / px_m);
+      let s = Math.floor((last_time - d * px_d - h * px_h - m * px_m) / px_s);
+      let r = [];
       if (d > 0) {
-        result.push(`<em>${d}</em>天`);
+        r.push(`<em>${d}</em>天`);
       }
-      if (h > 0) {
-        result.push(`<em>${h}</em>时`);
+      if (r.length || (h > 0)) {
+        r.push(`<em>${h}</em>时`);
       }
-      if (m > 0) {
-        result.push(`<em>${m}</em>分`);
+      if (r.length || m > 0) {
+        r.push(`<em>${m}</em>分`);
       }
-      if (s > 0) {
-        result.push(`<em>${s}</em>秒`);
+      if (r.length || s > 0) {
+        r.push(`<em>${s}</em>秒`);
       }
-
-      self.lastTime = result.join('');
+      self.last_time = r.join('');
       update.call(self, r.join(''));
-      setTimeout(() => {
+      setTimeout(function () {
         self.countdown(end, update, handle);
       }, 1000);
     }
